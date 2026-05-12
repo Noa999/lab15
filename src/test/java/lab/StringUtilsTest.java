6c5b44e07f202b91de1ff197f6fcbb4f65307997
Noa Zaya <norazaya1@gmail.com>
2026-05-12 15:24:46 +0800

    feat: add StringUtils.reverse

---
+++ src/test/java/lab/StringUtilsTest.java
@@ -0,0 +1,27 @@
+package lab;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    @Test
+    void reverse_empty() {
+        assertEquals("", StringUtils.reverse(""));
+    }
+
+    @Test
+    void reverse_single() {
+        assertEquals("a", StringUtils.reverse("a"));
+    }
+
+    @Test
+    void reverse_ascii() {
+        assertEquals("olleh", StringUtils.reverse("hello"));
+    }
+
+    @Test
+    void reverse_null() {
+        assertNull(StringUtils.reverse(null));
+    }
+}
